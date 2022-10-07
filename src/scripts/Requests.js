bc4b62bbec5c9abf64e2cdd97164e63781af3120
Peggy Carambas <PCarambas@gmail.com>
2022-10-07 10:53:25 -0500

    first commit

---
+++ src/scripts/Requests.js
@@ -0,0 +1,73 @@
+import { getRequests, saveCompletion } from "./dataAccess.js"
+import { deleteRequest } from "./dataAccess.js"
+import { getPlumbers } from "./dataAccess.js"
+
+
+const mainContainer = document.querySelector("#container")
+
+mainContainer.addEventListener("click", click => {
+    if (click.target.id.startsWith("request--")) {
+        const [, requestId] = click.target.id.split("--")
+        deleteRequest(parseInt(requestId))
+    }
+})
+
+mainContainer.addEventListener(
+    "change",
+    (event) => {
+        if (event.target.id === "plumbers") {
+            const [requestId, plumberId] = event.target.value.split("--")
+
+            /*
+                This object should have 3 properties
+                   1. requestId
+                   2. plumberId
+                   3. date_created
+            */
+            const completion = {
+                requestId: `${requestId}`,
+                plumberId: `${plumberId}`,
+                timestamp: Date.now()
+            }
+
+            /*
+                Invoke the function that performs the POST request
+                to the `completions` resource for your API. Send the
+                completion object as a parameter.
+             */
+            saveCompletion(completion)
+        }
+    }
+)
+
+export const Requests = () => {
+
+    const requests = getRequests()
+    const plumbers = getPlumbers()
+
+    let html = `<ul>`
+    const listItems = requests.map(request => {
+
+        return `<li id="${request.id}">
+                ${request.description}
+                <button class="request__delete" id="request--${request.id}">
+                    Delete
+                </button>
+
+                <select class="plumbers" id="plumbers">
+                <option value="0">Choose</option>
+                    ${plumbers.map(
+            plumber => {
+                return `<option value="${request.id}--${plumber.id}">${plumber.name}</option>`
+            }
+        ).join("")
+            }
+            </select>
+            </li>`
+    })
+
+    html += listItems.join("")
+
+    html += `</ul>`
+    return html
+}
---
+++ src/scripts/dataAccess.js
@@ -0,0 +1,107 @@
+const applicationState = {
+    requests: [],
+    plumbers: [],
+    completions: []
+    
+}
+
+export const saveCompletion = (completedRequest) => {
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(completedRequest)
+
+    }
+
+    return fetch(`${API}/completions`, fetchOptions)
+    .then(response => response.json())
+    .then(
+        (completed) => {
+            // Store the external state in application state
+            applicationState.completions = completed
+        }
+    )
+}
+
+
+export const fetchCompletions = () => {
+    return fetch(`${API}/completions`)
+    .then(response => response.json())
+    .then(
+        (completed) => {
+            // Store the external state in application state
+            applicationState.completions = completed
+        }
+    )
+    
+}
+
+
+const API = "http://localhost:8088"
+
+// Original copy and paste from ch 9 was fetchRequests
+
+export const fetchPlumbers = () => {
+    return fetch(`${API}/plumbers`)
+        .then(response => response.json())
+        .then(
+            (data) => {
+                applicationState.plumbers = data
+                
+            }
+        )
+}
+
+export const fetchRequests = () => {
+    return fetch(`${API}/requests`)
+        .then(response => response.json())
+        .then(
+            (serviceRequests) => {
+                // Store the external state in application state
+                applicationState.requests = serviceRequests
+            }
+        )
+}
+
+const mainContainer = document.querySelector("#container")
+
+export const sendRequest = (userServiceRequest) => {
+    const fetchOptions = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(userServiceRequest)
+    }
+
+
+    return fetch(`${API}/requests`, fetchOptions)
+        .then(response => response.json())
+        .then(() => {
+            mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
+
+        })
+}
+
+export const deleteRequest = (id) => {
+    return fetch(`${API}/requests/${id}`, { method: "DELETE" })
+        .then(
+            () => {
+                mainContainer.dispatchEvent(new CustomEvent("stateChanged"))
+            }
+        )
+}
+
+
+
+
+
+export const getRequests = () => {
+    return applicationState.requests.map(request => ({ ...request }))
+}
+
+export const getPlumbers = () => {
+    return applicationState.plumbers.map(plumber => ({ ...plumber }))
+}
